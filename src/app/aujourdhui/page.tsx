08d489d36203ce8f6f415c9e6e984bcea1367692
Olivier <olivier.fin74@gmail.com>
2026-05-12 16:46:49 +0200

    fix(routing): restore aujourdhui app route

---
+++ src/app/aujourdhui/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import NexialApp from '../../../nexial-app-complete'
+
+export default function AujourdhuiPage() {
+  return (
+    <div style={{ minHeight: '100vh', backgroundColor: '#FBF9F4' }}>
+      <NexialApp />
+    </div>
+  )
+}
